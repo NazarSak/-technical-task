e3da74f5929fc895eb17e799299ca8de97ebda46
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-05-09 21:53:43 +0300

    refresh load more

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { UserItem } from 'components/UserItem/UserItem';
 import styles from './ul.module.css';
-import { useState } from 'react';
-import { CARDS_PER_PAGE } from 'config/constant';
 import { LoadmoreBtn } from 'components/loadmoreBtn/LoadmoreBtn';
+import { useState } from 'react';
 
 export const Users = ({ users, setUsers }) => {
-  const [page, setPage] = useState(1);
-  const [usersToShow, setUsersToShow] = useState(users.slice(0, CARDS_PER_PAGE * page));
+  const [sliceUser, setSliceUser] = useState(users.slice((1, 37)));
 
-  const loadMore = () => {
-  setPage(page + 1);
-  setUsersToShow(users.slice(0, CARDS_PER_PAGE * (page + 1)));
-};
+  const addsliceUser = users.slice((1, 8));
+
+  const onClick = () => {
+    setSliceUser(...sliceUser, ...addsliceUser);
+    console.log(sliceUser);
+  };
 
   return (
     <ul className={styles.list}>
       {users.map(user => {
         return <UserItem key={user.id} user={user} setUsers={setUsers} />;
       })}
-      {usersToShow.length < users.length && <LoadmoreBtn type="button" onClick={loadMore}/>}
+      <LoadmoreBtn onClick={onClick} />
     </ul>
   );
 };
--- src/pages/TweetsPage.jsx
+++ src/pages/TweetsPage.jsx
@@ -1,42 +1,37 @@
-import {useEffect, useState} from "react";
-import { getUser } from "apiUsers/services";
-import { Users } from "components/users/users";
+import { useEffect, useState } from 'react';
+import { getUser } from 'apiUsers/services';
+import { Users } from 'components/users/users';
 
-
- export const  TweetPage = () => {
+export const TweetPage = () => {
   const [users, setUsers] = useState([]);
- 
+
   const usersStorage = JSON.parse(localStorage.getItem('users'));
 
   useEffect(() => {
     getUser()
-      .then((res) => {
+      .then(res => {
         if (res.ok) {
           return res.json();
         }
       })
-      .then((data) => {
-        const formattingUsers = data.map((item) => ({
+      .then(data => {
+        const formattingUsers = data.map(item => ({
           ...item,
           follow: false,
         }));
+        
         setUsers(formattingUsers);
-  
+      
+
         if (usersStorage === null) {
           localStorage.setItem('users', JSON.stringify(formattingUsers));
         }
       })
-      .catch((error) => console.log(error));
-      // eslint-disable-next-line
+      .catch(error => console.log(error));
+    // eslint-disable-next-line
   }, []);
 
-  // const onLoadMore = () => {
-  //   setPage(prevState => prevState + 1);
-  // };
 
+  return <Users users={usersStorage ?? users} setUsers={setUsers} />
 
-  return (
-    <Users users={usersStorage ?? users} setUsers={setUsers}/>
-  );
-}
-
+};
