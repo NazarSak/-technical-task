7cd146f25e5f2d2518be6eff6c661da8e21a6f52
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-05-09 22:16:06 +0300

    refresh

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,8 @@
 export const HomePage = () => {
   return (
     <div>
-      <h1>
-        Task manager welcome page
+      <h1 style={{fontSize:"30px",color:"black",textTransform:"uppercase"}}>
+        Welcome back
         <span role="img" aria-label="Greeting icon">
           💁‍♀️
         </span>
